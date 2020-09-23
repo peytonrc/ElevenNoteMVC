59d3a1f339a10daae1c8acf4a36148e88aaa5790
peytonrc <peytnrose@gmail.com>
2020-09-23 14:04:48 -0400

    Added GetNotes Method

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,58 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId; // Private field of type Guid called "_userId"
+
+        public NoteService(Guid userId) // Constructor
+        {
+            _userId = userId;
+        }
+
+        // Create Note Method
+        public bool CreateNote(NoteCreate model)
+        {
+            Note entity = new Note() // entity is the name given to the new Note instance
+            {
+                OwnerId = _userId,
+                Title = model.Title,
+                Content = model.Content,
+                CreatedUtc = DateTimeOffset.Now
+            };
+
+            using (ApplicationDbContext ctx = new ApplicationDbContext()) // ctx is the name given to the new ApplicationDbContext instance
+            {
+                ctx.Notes.Add(entity); // Notes is from Dbcontext and we are adding to the Note entity
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        // Get Notes Method
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (ApplicationDbContext ctx = new ApplicationDbContext())
+            {
+                var query = ctx
+                    .Notes
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(e =>
+                    new NoteListItem
+                    {
+                        NoteId = e.NoteId,
+                        Title = e.Title,
+                        CreatedUtc = e.CreatedUtc
+                    });
+                return query.ToArray();
+            }
+        }
+
+    }
+}
